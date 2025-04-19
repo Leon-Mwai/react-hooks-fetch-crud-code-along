8b5f0b984d0e73729204b5bd15a019fb91622177
leon mwai <leon.mwai@student.moringaschool.com>
2025-04-19 16:14:10 +0300

    components added

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,11 @@
+// src/components/Filter.js
 import React from "react";
 
 function Filter({ category, onCategoryChange }) {
   return (
     <div className="Filter">
-      <select
-        name="filter"
-        value={category}
-        onChange={(e) => onCategoryChange(e.target.value)}
-      >
-        <option value="All">Filter by category</option>
+      <select value={category} onChange={(e) => onCategoryChange(e.target.value)}>
+        <option value="All">All</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
         <option value="Dessert">Dessert</option>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,43 @@
+// src/components/Item.js
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
+
+  function handleAddToCartClick() {
+    const updatedItem = { ...item, isInCart: !item.isInCart };
+
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ isInCart: updatedItem.isInCart }),
+    })
+      .then((r) => r.json())
+      .then(onUpdateItem);
+  }
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => {
+        if (r.ok) {
+          onDeleteItem(item);
+        }
+      });
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
-        {item.isInCart ? "Remove From" : "Add to"} Cart
+      <button onClick={handleAddToCartClick} className="add">
+        {item.isInCart ? "Remove From Cart" : "Add to Cart"}
       </button>
-      <button className="remove">Delete</button>
+      <button onClick={handleDeleteClick} className="delete">
+        Delete
+      </button>
     </li>
   );
 }
